67fe94e482b4a91a2a64461c34866ede1e75b973
Thallysson <thallyssonklein@gmail.com>
2020-07-08 10:21:52 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# About this project
+
+This project is a monolithic repository made with the purpose to make easy for me make new Telegram bots from my ideas (using Flask + Continuous Integration + PythonAnyWhere)
+
+## carbonara_sh_bot
+### TODO
+
+- [ ] enable "inline" use
+- [ ] use the token that the telegram passes to the webhook instead of the token saved in an environment variable.
+- [ ] send the image directly to the telegram API without using img_bb as an intermediary
+- [ ] configure continuous integration
---
+++ application.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+if os.getenv('DEV'):
+    from flask_ngrok import run_with_ngrok
+    run_with_ngrok(app)
+
+@app.route('/carbonara_webhook/', methods=['POST'])
+def carbonara_webhook():
+    update = request.get_json()
+    message = update.get('message')
+    if message:
+        image = requests.post(url='https://carbonara.now.sh/api/cook', json={'code' : message['text'], 'backgroundColor': '#1F816D'}).content
+        img_bb = requests.post(url='https://api.imgbb.com/1/upload?expiration=60&key=' + os.getenv('IMGBB_KEY'), files={'image' : image}).text
+        t_request = requests.post(url='https://api.telegram.org/bot' + os.getenv('TELEGRAM_TOKEN') + '/sendPhoto', json={'chat_id' : message['chat']['id'], 'photo' : json.loads(img_bb)['data']['url']})
+    return 'OK', 200
+if __name__ == '__main__':
+    app.run()
+
